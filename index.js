afe17db84f10ffd4e2ded7f31ece5cd8649f9f36
Pablo Varela <pablovarela182@gmail.com>
2018-02-23 19:55:22 +0100

    Refactor: Waaaay easier way to handle color groups

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const contrast = require('contrast')
 
-const SCHEME_LENGTH = 4
-
 const defaults = {
   bg: '000000',
   fg: 'ffffff',
@@ -14,7 +12,7 @@ const fillScheme = scheme => {
   }
 
   const newScheme = []
-  for (let i = 0; i < SCHEME_LENGTH; i++) {
+  for (let i = 0; i < fgTypes.length; i++) {
     if (i < scheme.length) {
       newScheme.push(scheme[i])
     } else {
@@ -24,6 +22,58 @@ const fillScheme = scheme => {
 
   return newScheme
 }
+
+const fgTypes = [
+  [
+    'StorageClass',
+    'Type',
+    'Identifier',
+    'Delimiter',
+    'jsBraces',
+    'jsFuncBraces',
+    'jsIfElseBraces',
+    'jsTryCatchBraces',
+    'jsModuleBraces',
+    'jsObjectBraces',
+    'jsFinallyBraces',
+    'jsSwitchBraces',
+    'jsTemplateBraces',
+    'jsParens',
+    'jsFuncParens',
+    'jsBrackets'
+  ],
+  [
+    'Function',
+    'Include',
+    'Constant',
+    'Number',
+    'SpecialChar'
+  ],
+  [
+    'String',
+    'Label',
+    'Repeat',
+    'Conditional',
+    'StatusLine'
+  ],
+  [
+    'Character',
+    'Exception',
+    'Statement',
+    'Boolean',
+    'Keyword',
+    'Special',
+    'Operator'
+  ]
+]
+
+const getFgConfig = ({ scheme, bg }) =>
+    fgTypes.map((typeArr, typeIndex) =>
+      typeArr.reduce((acc, curr) =>
+        `${acc}
+hi ${curr} guifg=#${scheme[typeIndex]} guibg=#${bg}
+`
+      , '')).join('')
 
 module.exports = (name, colors) => {
   if (!name || typeof name !== 'string') {
@@ -40,36 +90,6 @@ hi LineNr guifg=#5e6c70 guibg=#${bg} guibg=#${bg}
 hi Comment guifg=#5e6c70 guibg=#${bg} guibg=#${bg}
 hi StorageClass guifg=#${scheme[0]} guibg=#${bg}
 hi Type guifg=#${scheme[0]} guibg=#${bg}
-hi Identifier guifg=#${scheme[0]} guibg=#${bg}
-hi Delimiter guifg=#${scheme[0]} guibg=#${bg}
-hi jsBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsFuncBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsIfElseBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsTryCatchBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsModuleBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsObjectBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsFinallyBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsSwitchBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsTemplateBraces guifg=#${scheme[0]} guibg=#${bg}
-hi jsParens guifg=#${scheme[0]} guibg=#${bg}
-hi jsFuncParens guifg=#${scheme[0]} guibg=#${bg}
-hi jsBrackets guifg=#${scheme[0]} guibg=#${bg}
-hi Function guifg=#${scheme[1]} guibg=#${bg}
-hi Include guifg=#${scheme[1]} guibg=#${bg}
-hi Constant guifg=#${scheme[1]} guibg=#${bg}
-hi Number guifg=#${scheme[1]} guibg=#${bg}
-hi SpecialChar guifg=#${scheme[1]} guibg=#${bg}
-hi String guifg=#${scheme[2]} guibg=#${bg}
-hi Label guifg=#${scheme[2]} guibg=#${bg}
-hi Repeat guifg=#${scheme[2]} guibg=#${bg}
-hi Conditional guifg=#${scheme[2]} guibg=#${bg}
-hi StatusLine guifg=#${scheme[2]} guibg=#${bg}
-hi Character guifg=#${scheme[3]} guibg=#${bg}
-hi Exception guifg=#${scheme[3]} guibg=#${bg}
-hi Statement guifg=#${scheme[3]} guibg=#${bg}
-hi Boolean guifg=#${scheme[3]} guibg=#${bg}
-hi Keyword guifg=#${scheme[3]} guibg=#${bg}
-hi Special guifg=#${scheme[3]} guibg=#${bg}
-hi Operator guifg=#${scheme[3]} guibg=#${bg}
+${getFgConfig({ scheme, bg })}
 `
 }
